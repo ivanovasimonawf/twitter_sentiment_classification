304293ad843005f8c87ac5ec43c273a9795b0ded
Simona Ivanova <simona.ivanova@webfactory.mk>
2018-05-08 12:43:21 +0200

    removed all preprocessing methods (the file we read is already preprocessed)

--- read_data.py
+++ read_data.py
@@ -2,8 +2,10 @@ import numpy as np
 from io import open
 from twitter_corpus import TwitterSentimentCorpus
 
-train_file = 'data/kaggle_data/train_kaggle.tsv'
-test_file =  'data/kaggle_data/test_kaggle.tsv'
+option = 'full'
+
+train_file = 'data/punctuation/train_kaggle_' + option + '.tsv'
+test_file =  'data/punctuation/test_kaggle_' + option + '.tsv'
 glove_file = 'glove/glove.6B.300d.txt'
 
 all_categories = []
@@ -11,6 +13,9 @@ train_data_lines = []
 test_data_lines = []
 twitter_corpus = TwitterSentimentCorpus()
 
+
+print(train_file)
+print(test_file)
 
 # separate data into tweets and labels
 def separate_data_labels(dataset):
@@ -24,32 +29,12 @@ def separate_data_labels(dataset):
     return data, labels
 
 
-# remove tweets with len 0
-def remove_tweets_at_indexes(tweets, labels, indexes):
-    sub = 0
-    for index in indexes:
-        index -= sub
-        del tweets[index]
-        del labels[index]
-        sub += 1
-    return tweets, labels
-
-
 # add all words from tweets in word2vec dictionary and remove any tweets that have len 0 (no glove vectos is available for them)
 def fill_twitter_corpus(tweets, labels):
-    indexes_to_remove = []
     for index in range(len(tweets)):
         if labels[index] not in all_categories:
             all_categories.append(labels[index])
-        if add_tweet_to_corpus(tweets[index]) == False:
-            indexes_to_remove.append(index)
-    tweets, labels = remove_tweets_at_indexes(tweets, labels, indexes_to_remove)
-    return tweets, labels
-
-
-# add words from a tweet in word2vec dictionary (returns True if no words were available as glove vectors)
-def add_tweet_to_corpus(tweet):
-    return twitter_corpus.add_sentence(tweet)
+        twitter_corpus.add_sentence(tweets[index])
 
 
 # read train/test data and prepare the twitter corpus
@@ -58,12 +43,12 @@ def prepare_data():
     global train_data_lines, test_data_lines
     twitter_corpus.create_word2vec_dictionary(glove_file)
     train_tweets, train_labels = \
-        separate_data_labels([line.split('\t') for line in open(train_file, encoding='utf-8').read().strip().split('\n')][1:])
+        separate_data_labels([line.split('\t') for line in open(train_file, encoding='utf-8').read().strip().split('\n')])
     test_tweets, test_labels = \
-        separate_data_labels([line.split('\t') for line in open(test_file, encoding='utf-8').read().strip().split('\n')][1:])
-    train_tweets, train_labels = fill_twitter_corpus(train_tweets, train_labels)
+        separate_data_labels([line.split('\t') for line in open(test_file, encoding='utf-8').read().strip().split('\n')])
+    fill_twitter_corpus(train_tweets, train_labels)
+    fill_twitter_corpus(test_tweets, test_labels)
     train_data_lines = [[label, tweet] for tweet, label in zip(train_tweets, train_labels)]
-    test_tweets, test_labels = fill_twitter_corpus(test_tweets, test_labels)
     test_data_lines = [[label, tweet] for tweet, label in zip(test_tweets, test_labels)]
     twitter_corpus.model = {}
 
