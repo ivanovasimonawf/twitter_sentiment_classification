1da6a224fe4e87f004ea4c162465d660c7c5467f
Simona Ivanova <simona.ivanova@webfactory.mk>
2018-05-09 11:05:42 +0200

    adding word to word2vec dict (count was not increased)

--- twitter_corpus.py
+++ twitter_corpus.py
@@ -24,20 +24,23 @@ class TwitterSentimentCorpus:
             self.word2vec[word] = self.model[word]
         except:
             known_word = 1
-        return known_word
+        return False if known_word else True
 
 
     def add_word(self, word):
         if word not in self.word2index:
-            self.word2index[word] = self.n_words
-            self.word2count[word] = 1
-            self.index2word[self.n_words] = word
-            self.n_words += 1
             known_word = self.add_to_word2vec(word)
+            if known_word:
+                self.word2index[word] = self.n_words
+                self.word2count[word] = 1
+                self.index2word[self.n_words] = word
+                self.n_words += 1
         else:
-            self.word2count[word] += 1
             known_word = self.add_to_word2vec(word)
-        return True if known_word == 0 else False
+            if known_word:
+                self.word2count[word] += 1
+        return known_word
+
 
     def create_word2vec_dictionary(self, glove_file):
         f = open(glove_file, 'r')
